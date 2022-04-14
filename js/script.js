bdbb46e749990ef2cfe88cefbedc7678ee49d7b1
Tumtog <94124944+Tumtog@users.noreply.github.com>
2022-04-14 12:59:19 +0300

    Desktop version

---
+++ js/script.js
@@ -0,0 +1,75 @@
+'use strict'
+//===========================================================Меню бургер
+let menuBtn = document.querySelector('.header__buttons-drop-icon');
+let menu = document.querySelector('.header__buttons-drop-body');
+
+menuBtn.addEventListener('click', function () {
+	// document.body.classList.toggle('lock');
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+//===========================================================
+
+//=========================================================== Слайдер
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 5,
+	spaceBetween: 0,
+	grabCursor: true,
+	centeredSlides: true,
+	// Responsive breakpoints
+	breakpoints: {
+		// when window width is >= 320px
+		// 320: {
+		// 	slidesPerView: 1,
+		// 	spaceBetween: 10,
+		// },
+		// 465: {
+		// 	slidesPerView: 2,
+		// 	spaceBetween: 10,
+		// },
+		// 768: {
+		// 	slidesPerView: 4,
+		// 	spaceBetween: 10,
+		// },
+		// 1024: {
+		// 	slidesPerView: 5,
+		// 	spaceBetween: 15,
+		// },
+		// 1440: {
+		// 	slidesPerView: 7,
+		// 	spaceBetween: 20,
+		// },
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	// // If we need pagination
+	// pagination: {
+	// 	el: '.swiper-pagination',
+	// },
+
+	//Управление слайдером клавиатурой
+	keyboard: {
+		//включить / выключить
+		enabled: true,
+		onlyInViewport: true,
+	},
+
+	// autoplay: {
+	// 	delay: 3000,
+	// 	stopOnLastSlide: false,
+	// 	disableOnInteraction: true,
+	// },
+
+	// // And if we need scrollbar
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// },
+});
+//===========================================================
